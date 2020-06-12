a02b708fcda59e37ef61bca6aa6c327231d28e10
radulupan <radul@q4websystems.com>
2020-06-12 08:15:42 -0400

    Added NSG and vnet with frontend subnet

--- az-vnet/main.tf
+++ az-vnet/main.tf
@@ -1,4 +1,39 @@
 provider "azurerm" {
     version = "2.0.0"
     subscription_id = var.subscriptionID
+    features {}
+}
+
+resource "azurerm_network_security_group" "frontend-nsg" {
+  name                = "frontend-nsg"
+  location            = var.location
+  resource_group_name = var.resourceGroupName
+}
+
+resource "azurerm_network_security_rule" "allow-tcp-80-rule" {
+  name                        = "allow-tcp-80"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  description                 = "Allows HTTP from anywhere"  
+  resource_group_name         = var.resourceGroupName
+  network_security_group_name = azurerm_network_security_group.frontend-nsg.name
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "virtualNetwork1"
+  location            = var.location
+  resource_group_name = var.resourceGroupName
+  address_space       = [var.vNetAddressSpace]
+  
+  subnet {
+    name           = "frontend"
+    address_prefix = var.frontEndSubnetAddressPrefix
+  }
+  
 }
