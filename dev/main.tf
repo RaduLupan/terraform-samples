bdd08731197d9079bfe66b928a94b27bb11bf620
RaduLupan <github@lupan.ca>
2020-06-22 08:13:41 -0400

    Dev environment does not need the same folder structure as modules, main.tf simply calls all child modules to deploy dev resources.

---
+++ dev/main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+    version         = "2.0.0"
+    subscription_id = var.subscriptionID
+    features {}
+}
+
+module "vnet" {
+    source = "../modules/vnet"
+    
+    subscriptionID              = var.subscriptionID
+    location                    = "eastus2"
+    vNetAddressSpace            = "10.0.0.0/16"
+    frontEndSubnetAddressPrefix = "10.0.0.0/24"
+    allowedSshAddressPrefix     = "135.23.87.216/32"
+}
+
+module "lb" {
+    source = "../modules/web-tier/lb"
+    
+    subscriptionID              = var.subscriptionID
+    location                    = "eastus2"
+    resourceGroup               = module.vnet.rg-name 
+}
+
