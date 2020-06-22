0cc33e3f6ed86a783bec7448df0de31355068d20
RaduLupan <github@lupan.ca>
2020-06-22 07:34:27 -0400

    Creates Azure LB Standard SKU-Initial Commit

---
+++ az-lb/variables.tf
@@ -0,0 +1,15 @@
+variable "subscriptionID" {
+    type = string
+    description = "Target subscription ID"
+}
+
+variable "location" {
+    type = string
+    description = "Location of your resource group"
+}
+
+variable "resourceGroup" {
+    type = string
+    description = "Name of your resource group"
+}
+
