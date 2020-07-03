92eaec964d7cefcc746a218ed8228abad9c41a20
RaduLupan <github@lupan.ca>
2020-07-03 09:35:49 -0400

    Added service point for Microsoft.KeyVault to frontend subnet

--- examples/az-vnet/main.tf
+++ examples/az-vnet/main.tf
@@ -67,6 +67,7 @@ resource "azurerm_subnet" "subnet1" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefix       = var.frontEndSubnetAddressPrefix
+  service_endpoints    =["Microsoft.KeyVault"] 
 }
 
 resource "azurerm_subnet_network_security_group_association" "frontend-nsg-association" {
