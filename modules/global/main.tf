f94b79881d275a902cbca4525826af2bdcd81863
RaduLupan <github@lupan.ca>
2020-07-09 08:14:40 -0400

    Added key vault policies and comment based help

--- modules/global/main.tf
+++ modules/global/main.tf
@@ -1,3 +1,8 @@
+# This template deploys the following Azure resources:
+# - 1 x Key Vault in existing resource group
+# - The Key Vault firewall allows traffic from existing vnet subnet via service endpoint
+# - Key Vault Access Policies for multiple existing VMs allowing them to access keys/secrets/certificates
+
 # Terraform 0.12 syntax is used so 0.12 is the minimum required version
 terraform {
   required_version = ">= 0.12"
@@ -50,4 +55,39 @@ data "azurerm_virtual_machine" "web" {
   
   name                = "vm-${var.serverName}-${count.index}"
   resource_group_name = var.resourceGroup
+}
+
+resource "azurerm_key_vault_access_policy" "web_key_vault_access_policy" {
+  count = var.vmNumber
+
+  key_vault_id = azurerm_key_vault.az_key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  
+  # In order to be able to export the identity of a VM I had to upgrade the provider version to 2.10.0.
+  # https://github.com/terraform-providers/terraform-provider-azurerm/pull/6826
+  # Also, had to check the properties of a VM in terraform.tfstate file to figure out that identity is a list, identity[0] is the first object in that list
+  # and principal_id is one of its properties.
+  # Finally, in order to resolve ${count.index} you need to enclose it in quotes, NOT the entire expresion as a Powershell user might think!
+  object_id    = data.azurerm_virtual_machine.web["${count.index}"].identity[0].principal_id
+  
+  key_permissions = [
+    "get",
+    "list",
+    "create",
+    "update",
+  ]
+
+  secret_permissions = [
+    "get",
+    "list",
+    "set",
+    "restore",
+  ]
+
+  certificate_permissions = [
+    "get",
+    "list",
+    "create",
+    "update",
+  ]
 }
