ddb6ed6f73612a608dcde20b096a25b690db63bd
RaduLupan <github@lupan.ca>
2020-11-24 08:26:22 -0500

    Upgraded to latest azurerm provider, allowed public blob access on the storage account

--- examples/az-cdn/main.tf
+++ examples/az-cdn/main.tf
@@ -2,76 +2,85 @@
 # - 1 x Storage Account with 1 x blob container
 # - 1 x CDN profile with 1 x endpoint pointing to the blob storage for origin
 
-# Terraform 0.12 syntax is used so 0.12 is the minimum required version
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.37.0"
+    }
+  }
+
+  required_version = "~> 0.13.0"
 }
 
 provider "azurerm" {
-    version = "2.10.0"
-    subscription_id = var.subscriptionID
-    features {}
+  subscription_id = var.subscription_id
+  features {}
 }
 
 locals {
-    project = "terraform-samples"
+  project = "terraform-samples"
 }
 
 # The random string needed for injecting randomness in the storage account and blob container names.
 resource "random_string" "random" {
-  length = 4
+  length  = 4
   special = false
 }
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = "st${var.environment}${lower(random_string.random.result)}"
-  resource_group_name      = var.resourceGroup
+  resource_group_name      = var.resource_group
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   # Public access is required for CDN to be able to serve content from the blob container.
+  allow_blob_public_access = "true"
+  
   network_rules {
     default_action = "Allow"
   }
 
   tags = {
     environment = var.environment
-    project = "${local.project}"
+    project     = local.project
   }
 }
 
 resource "azurerm_storage_container" "container" {
-  name                  = "${local.project}-${var.environment}-${var.location}-${lower(random_string.random.result)}"
+  name = "${local.project}-${var.environment}-${var.location}-${lower(random_string.random.result)}"
+  
   # Blob access type means "anonymous read access for blobs only". 
   container_access_type = "blob"
+  
   storage_account_name  = azurerm_storage_account.storage_account.name
 }
 
 resource "azurerm_cdn_profile" "cdn_profile" {
   name                = "cdn-profile-${var.environment}-${lower(random_string.random.result)}"
   location            = var.location
-  resource_group_name = var.resourceGroup
-  sku                 = var.cdnSku
+  resource_group_name = var.resource_group
+  sku                 = var.cdn_sku
 
   tags = {
     environment = var.environment
-    project = "${local.project}"
+    project     = local.project
   }
 }
 
 resource "azurerm_cdn_endpoint" "cdn_endpoint" {
   # Creates the CDN endpoint name out of the domain name to ensure uniqueness, i.e. cdn-example-com
-  name                = join("-",["cdn",replace(var.cdnEndpointDomain,".","-")])
+  name                = join("-", ["cdn", replace(var.cdn_endpoint_domain, ".", "-")])
   profile_name        = azurerm_cdn_profile.cdn_profile.name
   location            = var.location
-  resource_group_name = var.resourceGroup
+  resource_group_name = var.resource_group
 
   origin {
-    name      = "cdn-endpoint-origin"
+    name = "cdn-endpoint-origin"
     # Connect CDN endpoint to blob storage origin. Access cached content via http://<endpoint-name>.azureedge.net/<myPublicContainer>/<BlobName>.
     host_name = azurerm_storage_account.storage_account.primary_blob_host
   }
-  
+
   origin_host_header = azurerm_storage_account.storage_account.primary_blob_host
 }
