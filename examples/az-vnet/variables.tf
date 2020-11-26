92d5563aadbe7b38ac4837aa7429fcfe268618f5
RaduLupan <github@lupan.ca>
2020-11-26 07:58:07 -0500

    Reorganized variables, deleted terraform.tfvars

--- examples/az-vnet/variables.tf
+++ examples/az-vnet/variables.tf
@@ -1,24 +1,42 @@
-variable "subscriptionID" {
-    type = string
-    description = "Target subscription ID"
+#----------------------------------------------------------------------------
+# REQUIRED PARAMETERS: You must provide a value for each of these parameters.
+#----------------------------------------------------------------------------
+
+variable "subscription_id" {
+  description = "Target subscription ID"
+  type        = string
 }
 
 variable "location" {
-    type = string
-    description = "Location of your resource group"
+  description = "The Azure region where the resources are being deployed"
+  type        = string
 }
 
-variable "vNetAddressSpace" {
-    type = string
-    description = "Address space for your vNet in CIDR notation"
+#---------------------------------------------------------------
+# OPTIONAL PARAMETERS: These parameters have resonable defaults.
+#---------------------------------------------------------------
+
+variable "environment" {
+  description = "Environment i.e. dev, test, stage, prod"
+  type        = string
+  default     = "dev"
 }
 
-variable "frontEndSubnetAddressPrefix" {
-    type = string
-    description = "Address prefix for your front end subnet in CIDR notation"
+variable "vnet_address_space" {
+  description = "Address space for your vNet in CIDR notation"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "allowedSshAddressPrefix" {
-    type = string
-    description = "Address prefix for your range of IPs allowed for SSH in CIDR notation"
+variable "frontend_subnet_address_prefix" {
+  description = "Address prefix for your front end subnet in CIDR notation"
+  type        = string
+  default     = "10.0.0.0/24"
+
+}
+
+variable "allowed_ssh_address_prefix" {
+  description = "Address prefix for your range of IPs allowed for SSH in CIDR notation"
+  type        = string
+  default     = null
 }
