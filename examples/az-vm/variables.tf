31ba326ebb797a591323168151fcae846c9b27e2
RaduLupan <github@lupan.ca>
2020-12-03 08:09:25 -0500

    Reorganized variables, deleted terraform.tfvars

--- examples/az-vm/variables.tf
+++ examples/az-vm/variables.tf
@@ -1,38 +1,63 @@
-variable "subscriptionID" {
-    description = "Target subscription ID"
-    type        = string
+#----------------------------------------------------------------------------
+# REQUIRED PARAMETERS: You must provide a value for each of these parameters.
+#----------------------------------------------------------------------------
+
+variable "subscription_id" {
+  description = "Target subscription ID"
+  type        = string
 }
 
 variable "location" {
-    description = "Location of your resource group"
-    type        = string
+  description = "The Azure region where the resources are being deployed"
+  type        = string
 }
 
-variable "resourceGroup" {
-    description = "Name of your resource group"
-    type        = string
-}
-
-variable "subnetId" {
-    description = "Subnet Id for subnet that VM will sit on"
-    type        = string
-}
-
-variable "serverName" {
-    description = "Name of your VM"
-    type        = string
-}
-
-variable "vmNumber" {
-    description = "Number of deployed VMs"
-}
-
-variable "vmAdminUser" {
+variable "vm_admin_user" {
     description = "Admin user name"
     type        = string
 }
 
-variable "vmAdminPassword" {
+variable "vm_admin_password" {
     description = "Admin password"
     type        = string
+}
+
+#---------------------------------------------------------------
+# OPTIONAL PARAMETERS: These parameters have resonable defaults.
+#---------------------------------------------------------------
+
+variable "environment" {
+  description = "Environment i.e. dev, test, stage, prod"
+  type        = string
+  default     = "dev"
+}
+
+variable "vnet_resource_group" {
+  description = "Name of the resource group for existing vNet (if null new resource group is created)"
+  type        = string
+  default     = null
+}
+
+variable "vnet_name" {
+    description = "The name for existing vNet (if null new vNet is created)"
+    type        = string
+    default     = null
+}
+
+variable "subnet_name" {
+    description  = "The name for the subnet that the VMs will sit on (if null new subnet is created)"
+    type         = string
+    default      = null
+}
+
+variable "server_name" {
+    description = "Name of your VM"
+    type        = string
+    default     = "ubuntu"
+} 
+
+variable "vm_count" {
+    description = "Number of deployed VMs"
+    type        = number
+    default     = 2
 }
