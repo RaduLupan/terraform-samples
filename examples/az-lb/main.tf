c0e1878ca7223d5d4315112cb3cacc6b16437afb
RaduLupan <github@lupan.ca>
2020-12-04 07:29:30 -0500

    Updated azurerm provider version, pinned terraform version to 0.13.x, introduced conditional resource group in case var.resource_group is null

--- examples/az-lb/main.tf
+++ examples/az-lb/main.tf
@@ -1,54 +1,85 @@
 # This template deploys the following Azure resources:
 # - 1 x Load Balancer with 1 x Public IP, 1 x Backend Pool, 1 x Probe and 1 x Rule
 
-# Terraform 0.12 syntax is used so 0.12 is the minimum required version
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.37.0"
+    }
+  }
+
+  required_version = "~> 0.13.0"
 }
 
 provider "azurerm" {
-    version = "2.0.0"
-    subscription_id = var.subscriptionID
-    features {}
+  subscription_id = var.subscription_id
+  features {}
+}
+
+# Local calculated variables
+locals {
+  project        = "terraform-samples"
+  resource_group = var.resource_group == null ? azurerm_resource_group.rg[0].name : var.resource_group
+}
+
+# Create Resource Group if var.resource_group is null
+resource "azurerm_resource_group" "rg" {
+  count = var.resource_group == null ? 1 : 0
+
+  name     = "rg-${lower(replace(var.location, " ", ""))}-${local.project}-${var.environment}"
+  location = var.location
+
+  tags = {
+    environment = var.environment
+    project     = local.project
+    terraform   = "true"
+  }
 }
 
 resource "azurerm_public_ip" "pip01" {
   name                = "lb-pip"
-  resource_group_name = var.resourceGroup
+  resource_group_name = local.resource_group
   location            = var.location
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 resource "azurerm_lb" "web_loadbalancer" {
-  name                = "web-loadbalancer"
+  name                = "lb-web"
   location            = var.location
-  resource_group_name = var.resourceGroup
+  resource_group_name = local.resource_group
   sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "lb-public-ip"
     public_ip_address_id = azurerm_public_ip.pip01.id
   }
+
+  tags = {
+    environment = var.environment
+    project     = local.project
+    terraform   = "true"
+  }
 }
 
 resource "azurerm_lb_backend_address_pool" "web_lb_pool" {
-  resource_group_name = var.resourceGroup
+  resource_group_name = local.resource_group
   loadbalancer_id     = azurerm_lb.web_loadbalancer.id
   name                = "lb-backend-pool"
 }
 
 resource "azurerm_lb_probe" "web_lb_probe" {
-  resource_group_name = var.resourceGroup
+  resource_group_name = local.resource_group
   loadbalancer_id     = azurerm_lb.web_loadbalancer.id
   name                = "http-running-probe"
   port                = 80
-  protocol            = "Http"   
+  protocol            = "Http"
   request_path        = "/"
 }
 
 resource "azurerm_lb_rule" "web_lb_http_rule" {
-  resource_group_name            = var.resourceGroup
+  resource_group_name            = local.resource_group
   loadbalancer_id                = azurerm_lb.web_loadbalancer.id
   name                           = "lb-http-rule"
   protocol                       = "Tcp"
@@ -56,5 +87,5 @@ resource "azurerm_lb_rule" "web_lb_http_rule" {
   backend_port                   = 80
   frontend_ip_configuration_name = "lb-public-ip"
   backend_address_pool_id        = azurerm_lb_backend_address_pool.web_lb_pool.id
-  probe_id                       = azurerm_lb_probe.web_lb_probe.id   
+  probe_id                       = azurerm_lb_probe.web_lb_probe.id
 }
