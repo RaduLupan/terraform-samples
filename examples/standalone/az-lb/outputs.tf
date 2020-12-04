9589ad68aeef1e62eff800b8dfbc07b00b435130
RaduLupan <github@lupan.ca>
2020-12-04 07:51:04 -0500

    Added output for load balancer backend pool id

--- examples/standalone/az-lb/outputs.tf
+++ examples/standalone/az-lb/outputs.tf
@@ -2,3 +2,8 @@ output "lb_pip" {
   description = "The public IP of the load balancer"
   value       = azurerm_public_ip.pip01.ip_address
 }
+
+output "lb_backend_pool_id" {
+  description = "The ID of the load balancer backend pool"
+  value       = azurerm_lb_backend_address_pool.web_lb_pool.id
+}
