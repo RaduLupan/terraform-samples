70497c3eecc8e9c64b1160aceddb21d481db481c
RaduLupan <github@lupan.ca>
2020-12-09 08:11:51 -0500

    Separated outputs

---
+++ modules/network/outputs.tf
@@ -0,0 +1,19 @@
+output "rg_name" {
+  description = "The name of the resource group the vNet is in"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "vnet_name" {
+  description = "The name of the vNet"
+  value       = azurerm_virtual_network.vnet1.name
+}
+
+output "fe_subnet_id" {
+  description = "The ID of the frontend subnet"
+  value       = azurerm_subnet.subnet1.id
+}
+
+output "nsg_id" {
+  description = "The ID of the network security group associated with the subnet"
+  value       = azurerm_network_security_group.frontend_nsg.id
+}
