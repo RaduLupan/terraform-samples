853367c31385b6e07d01cc76fb552c1b59946e5e
RaduLupan <github@lupan.ca>
2020-12-11 08:18:21 -0500

    Derived from old global module

---
+++ modules/key-vault/main.tf
@@ -0,0 +1,55 @@
+# This template deploys the following Azure resources:
+# - 1 x Key Vault in existing resource group
+# - The Key Vault firewall allows traffic from existing vnet subnet via service endpoint
+
+terraform {
+  required_version = "~> 0.13.0"
+}
+
+locals {
+  common_tags = {
+    terraform   = true
+    environment = var.environment
+    project     = "terraform-samples-modules"
+    role        = "key-vault"
+  }
+}
+
+# The random string needed for injecting randomness in the name for storage account, blob container and key vault.
+resource "random_string" "random" {
+  length  = 4
+  special = false
+}
+
+## Use this data source to access the configuration of the AzureRM provider.
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "az_key_vault" {
+  name                        = "kv-${var.environment}-${lower(random_string.random.result)}"
+  location                    = var.location
+  resource_group_name         = var.resource_group
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_enabled         = true
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  # Boolean flag to specify whether Azure Virtual Machines are permitted to retrieve certificates stored as secrets from the key vault. Defaults to false.
+  enabled_for_deployment = true
+
+  # Uncomment this line if Azure Disk Encryption is permitted to retrieve secrets from the vault and unwrap keys.
+  # enabled_for_disk_encryption = true
+
+  # Uncomment this line if Azure Resource Manager is permitted to retrieve secrets from the key vault.
+  # enabled_for_template_deployment = true
+
+  network_acls {
+    default_action             = "Deny"
+    bypass                     = "AzureServices"
+    virtual_network_subnet_ids = var.subnet_ids
+  }
+
+  tags = local.common_tags
+}
+
