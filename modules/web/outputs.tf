9cab73f3909ff52d6e0cea2c3ff02b2daccca283
RaduLupan <github@lupan.ca>
2020-12-14 07:24:13 -0500

    Added outputs

--- modules/web/outputs.tf
+++ modules/web/outputs.tf
@@ -1,9 +1,9 @@
 output "vm_names" {
   description = "The VM names"
-  value       = module.web.vm_names
+  value       = azurerm_virtual_machine.web_vm[*].name
 }
 
 output "lb_pip" {
   description = "The public IP of the load balancer"
-  value       = module.web.lb_pip
+  value       = azurerm_public_ip.pip1.ip_address
 }
