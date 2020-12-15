5b6ee4b13b8af1d0bd806d8449b8820a08c219ab
RaduLupan <github@lupan.ca>
2020-12-15 08:07:43 -0500

    Added key vault id as output in the key-vault module and corresponding example

--- examples/key-vault/outputs.tf
+++ examples/key-vault/outputs.tf
@@ -2,3 +2,8 @@ output "vault_uri" {
     description = "The URI of the Key Vault"
     value       = module.key-vault.vault_uri
 }
+
+output "vault_id" {
+    description = "The ID of the Key Vault"
+    value       = module.key-vault.vault_id
+}
--- modules/key-vault/outputs.tf
+++ modules/key-vault/outputs.tf
@@ -2,3 +2,8 @@ output "vault_uri" {
     description = "The URI of the Key Vault"
     value       = azurerm_key_vault.az_key_vault.vault_uri
 }
+
+output "vault_id" {
+    description = "The ID of the Key Vault"
+    value       = azurerm_key_vault.az_key_vault.id
+}
