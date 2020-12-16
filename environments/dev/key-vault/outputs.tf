8176190f333027e3746785bc977ff84cf56df800
RaduLupan <github@lupan.ca>
2020-12-16 09:28:56 -0500

    Uncommented outputs

--- environments/dev/key-vault/outputs.tf
+++ environments/dev/key-vault/outputs.tf
@@ -1,4 +1,4 @@
-/*output "vault_uri" {
+output "vault_uri" {
     description = "The URI of the Key Vault"
     value       = module.key-vault.vault_uri
 }
@@ -6,4 +6,4 @@
 output "vault_id" {
     description = "The ID of the Key Vault"
     value       = module.key-vault.vault_id
-}*/
+}
